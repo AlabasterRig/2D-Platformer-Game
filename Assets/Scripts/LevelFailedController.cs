f90762deccca34b72ca5381ca3cb5d6dc7b29949
Utkrist Jaiswal <utkrist.j@gmail.com>
2025-05-02 03:29:27 +0530

    Added Death Trigger below platform

---
+++ Assets/Scripts/LevelFailedController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFailed : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("Player Died from falling!");
+        }
+    }
+}
