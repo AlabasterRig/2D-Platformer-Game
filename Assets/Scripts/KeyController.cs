3ba8a2a147d644aad55a9570aa4233ab489eecd0
Utkrist Jaiswal <utkrist.j@gmail.com>
2025-05-03 04:27:14 +0530

    Added Fade-Out of Key on pickup

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -1,14 +1,49 @@
 using UnityEngine;
+using System.Collections;
 
 public class KeyController : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
             player.PickUpKey();
-            Destroy(gameObject);
+            collider.enabled = false;
+            StartCoroutine(AnimateAndDestroy());
         }
+    }
+
+    private IEnumerator AnimateAndDestroy()
+    {
+        float duration = 0.5f;
+        float elapsed = 0f;
+
+        Vector2 startPos = transform.position;
+        Vector2 endPos = startPos + Vector2.up * 1.0f;
+        Color startColor = spriteRenderer.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+
+            transform.position = Vector2.Lerp(startPos, endPos, t);
+            spriteRenderer.color = Color.Lerp(startColor, endColor, t);
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
