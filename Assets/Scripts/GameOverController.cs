a43a8a010c2d0bcd8300e72264b84d7dc62cf5e4
Utkrist Jaiswal <utkrist.j@gmail.com>
2025-05-05 03:46:23 +0530

    Added Restart and Quit button with functionalities

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -1,14 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverController : MonoBehaviour
 {
     public LevelController levelController;
     public Button RestartButton;
+    public Button QuitButton;
 
     private void Awake()
     {
         RestartButton.onClick.AddListener(RestartLevel);
+        QuitButton.onClick.AddListener(ReturnToMainMenu);
+    }
+
+    private void ReturnToMainMenu()
+    {
+        gameObject.SetActive(false);
+        SceneManager.LoadScene(0);
     }
 
     public void PlayerDied()
